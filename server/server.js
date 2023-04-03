d73e82a9f09876c99cc78265ea84ecb6414f9030
JoelEncinas <joelencinasmartin@gmail.com>
2023-04-03 23:41:42 +0200

    created crud for todos

--- server/server.js
+++ server/server.js
@@ -26,4 +26,28 @@ app.get("/todos", async (req, res) => {
   res.json(todos);
 });
 
+app.post("/todos/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.delete("/todos/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+
+  res.json({ result });
+});
+
+app.put("/todos/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.complete = !todo.complete;
+  todo.save();
+
+  res.json(todo);
+});
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
