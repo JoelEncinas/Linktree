4da5a6459dd93668875fb2b53a3701ea09d849d7
JoelEncinas <joelencinasmartin@gmail.com>
2023-04-03 23:51:32 +0200

    created html structure of todo app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-    return (
-      <div className="App">
-        <h1>Hello from react</h1>
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+
+  return (
+    <div className="App">
+      <h1>Hello from react</h1>
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+          <div className="text">get the bread</div>
+          <div className="delete-todo">x</div>
+        </div>
+
+        <div className="todo is-complete">
+          <div className="checkbox"></div>
+          <div className="text">get the milk</div>
+          <div className="delete-todo">x</div>
+        </div>
       </div>
-    );
-  }
-  
-  export default App;
+    </div>
+  );
+}
+
+export default App;
