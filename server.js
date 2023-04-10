6b8b8646cc020a1e6f8a2d4c8236143989f8482b
Joel Encinas <joelencinasmartin@gmail.com>
2023-04-10 21:51:06 +0200

    added handlebars and body-parser

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const hsb = require("hbs");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+hsb.registerPartials(__dirname + "/partials");
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.pug2uxj.mongodb.net/?retryWrites=true&w=majority`;
 mongoose
