fbb6224b292df5a3c1996c223fb234f2a283be57
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-01 02:07:49 +0200

    added ui info when user or credentials are wrong

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,20 +2,32 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
-import { Form, FormGroup, Input, Label } from "reactstrap";
+import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
 
 function Login() {
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [values, setValues] = useState({ username: "", password: "" });
+
+  const [submitted, setSubmitted] = useState(false);
+  const [invalidCredentials, setInvalidCredentials] = useState(false);
+  const [userNotFound, setUserNotFound] = useState(false);
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+    setSubmitted(false);
+    setInvalidCredentials(false);
+    setUserNotFound(false);
+  }
 
   function handleLogin(e) {
     e.preventDefault();
+    setSubmitted(true);
 
     const user = {
-      username: username,
-      password: password,
+      username: values.username,
+      password: values.password,
     };
 
     fetch("http://127.0.0.1:4997/auth/login", {
@@ -35,9 +47,9 @@ function Login() {
             navigate("/protected");
           });
         } else if (res.status === 401) {
-          console.log("invalid credentials");
+          setInvalidCredentials(true);
         } else if (res.status === 404) {
-          console.log("user not found");
+          setUserNotFound(true);
         } else if (res.status === 500) {
           console.log("server err");
         }
@@ -65,32 +77,61 @@ function Login() {
   }, [navigate]);
 
   return (
-    <div>
+    <Container>
       <Navbar />
-      <h1>Login</h1>
-      <Form onSubmit={(event) => handleLogin(event)}>
-        <FormGroup floating>
-          <Input
-            required
-            type="text"
-            value={username}
-            placeholder="Username"
-            onChange={(e) => setUsername(e.target.value)}
-            id="username"
-          />
-          <Label for="username">Username</Label>
-        </FormGroup>
-        <label htmlFor="password">Password:</label>
-        <input
-          required
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          id="password"
-        ></input>
-        <input type="submit" value="Login"></input>
-      </Form>
-    </div>
+      <h1 className="text-center">Login</h1>
+      <Container className="mx-auto" style={{ maxWidth: 400 }}>
+        <Form className="d-flex flex-column">
+          <FormGroup
+            className={submitted && userNotFound ? "has-danger" : ""}
+            floating
+          >
+            <Input
+              className={submitted && userNotFound ? "is-invalid" : ""}
+              required
+              type="text"
+              value={values.username}
+              placeholder="Username"
+              onChange={handleInputChange}
+              id="username"
+              name="username"
+            />
+            <Label for="username">Username</Label>
+            {submitted && userNotFound && (
+              <div className="invalid-feedback">Username not found.</div>
+            )}
+          </FormGroup>
+          <FormGroup
+            className={submitted && invalidCredentials ? "has-danger" : ""}
+            floating
+          >
+            <Input
+              className={submitted && invalidCredentials ? "is-invalid" : ""}
+              required
+              type="password"
+              value={values.password}
+              placeholder="Password"
+              onChange={handleInputChange}
+              id="password"
+              name="password"
+            />
+            <Label for="password">Password</Label>
+            {submitted && invalidCredentials && (
+              <div className="invalid-feedback">Invalid credentials.</div>
+            )}
+          </FormGroup>
+
+          <Button
+            color="primary"
+            className="mx-auto"
+            style={{ width: 100 }}
+            onClick={(event) => handleLogin(event)}
+          >
+            Login
+          </Button>
+        </Form>
+      </Container>
+    </Container>
   );
 }
 
