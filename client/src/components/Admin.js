4b72cdad60ffef0e21865b6864e89db3fa8e5c84
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-03 09:30:10 +0200

    created input to save bio

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
 
 function Admin() {
   const navigate = useNavigate();
   const location = useLocation();
 
   const [username, setUserName] = useState(null);
+  const [bioValue, setBioValue] = useState("");
+  const [counter, setCounter] = useState(0);
 
   function logout() {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
@@ -22,12 +25,53 @@ function Admin() {
     }
   }, [location.state, navigate]);
 
+  useEffect(() => {
+    setCounter(bioValue.length);
+  }, [bioValue]);
+
+  function handleInputChange(e) {
+    console.log(e)
+    const value = e.target.value;
+    setBioValue(value);
+  }
+
+  function handleProfile() {
+    // TODO
+  }
+
   return (
-    <div>
+    <>
       <button onClick={logout}>Logout</button>
-      <h1>Admin</h1>
-      {username && <p>Hello {username}</p>}
-    </div>
+      <Container className="mx-auto" style={{ maxWidth: 400 }}>
+        <h1 className="text-center">Profile</h1>
+        <Form className="d-flex flex-column" onSubmit={handleProfile}>
+          <FormGroup row floating>
+            <Input
+              required
+              type="textarea"
+              value={bioValue}
+              placeholder="Password"
+              onChange={handleInputChange}
+              id="bio"
+              name="bio"
+              maxLength={90}
+              style={{height: 105}}
+            />
+            <Label for="bio">Bio</Label>
+            <span>{counter}/90</span>
+          </FormGroup>
+
+          <Button
+            type="submit"
+            color="primary"
+            className="mx-auto"
+            style={{ width: 100 }}
+          >
+            Save
+          </Button>
+        </Form>
+      </Container>
+    </>
   );
 }
 
